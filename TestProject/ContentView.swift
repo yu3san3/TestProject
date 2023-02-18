09aa6c758d57a94196527c993993e68adc3f181e
NIWA-Yuichiro <20r5068@moon.aichi-u.ac.jp>
2023-02-18 22:21:54 +0900

    Initial Commit

---
+++ TestProject/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TestProject
+//
+//  Created by 丹羽雄一朗 on 2023/01/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by 丹羽雄一朗 on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
